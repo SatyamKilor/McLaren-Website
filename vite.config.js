0a02aa06dd3fbeefc328e771d3d328d93e1608a3
Satyam Kilor <satyamkilor8.com>
2025-06-05 14:42:00 +0530

    All set for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/McLaren-Website',
-})
+  base: '/McLaren-Website/', // 👈 must match your deployed subpath
+});
