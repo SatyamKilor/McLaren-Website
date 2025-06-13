4822196a1169e1d462b17b3619ea8d6bf1187330
Satyam Kilor <satyamkilor8.com>
2025-06-13 13:47:50 +0530

    fixes

--- src/main.js
+++ src/main.js
@@ -173,7 +173,7 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.x = 0;
 camera.position.y = 1.5;
-camera.position.z = 7.2;
+camera.position.z = 7.5;
 
 const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x8d8d8d, 3 );
 				hemiLight.position.set( 0, 20, 0 );
